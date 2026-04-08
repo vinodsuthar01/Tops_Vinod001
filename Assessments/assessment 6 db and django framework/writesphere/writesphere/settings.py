73d5d52eea4ceb37f0adba53a753b0b6be9988bd
Vinod Suthar <sutharv412@gmail.com>
2026-04-08 16:04:23 +0530

    update my blog

--- Assessments/assessment 6 db and django framework/writesphere/writesphere/settings.py
+++ Assessments/assessment 6 db and django framework/writesphere/writesphere/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = '/'
