30dde2d215b258f4ef4921980aca11239d7a9658
Vinod Suthar <sutharv412@gmail.com>
2026-06-11 17:51:15 +0530

    upload js-assignment session-4

--- Assignments/assignment_11 Java-Script Essentials and Advanced/Session_3/task1.js
+++
@@ -1,4 +0,0 @@
-let likes = 23;
-let comments = 11;
-
-console.log(likes+comments);
--- Assignments/assignment_11 Java-Script Essentials and Advanced/Session_3/task6.js
+++
@@ -1,7 +0,0 @@
-// Build a function getBadge(followers) that returns 'Verified Creator ⭐' if followers is 1000 or more, otherwise returns 'Regular User'. Use the ternary operator to implement this logic.
-
-let followers1 = 999;
-
-let verified = followers1>= 1000 ? "verified creator" : "regular user"
-
-console.log(verified);
