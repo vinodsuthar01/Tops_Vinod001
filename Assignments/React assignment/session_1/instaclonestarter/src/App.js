0ec04151222344d2c5ba70ba379f40a91124dcc2
Vinod Suthar <sutharv412@gmail.com>
2026-06-28 08:58:01 +0530

    react assignment-1

---
+++ Assignments/React assignment/session_1/instaclonestarter/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TrendingSong from './Components/TrendingSong';
+
+function App() {
+  return (
+     <>
+     <h1>Welcome to My React Zomato App</h1>
+     <TrendingSong/>
+     </>
+  );
+}
+
+export default App;
---
+++ Assignments/React assignment/session_1/instaclonestarter/src/Components/TrendingSong.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TrendingSong() {
+  return (
+    <h2>Trending on Spotify: Calm Down</h2>
+  )
+}
+
+export default TrendingSong
